e273d6ca2ce669d75820edfdf14faf51bba441d6
CrisMoreira2510 <106870383+CrisMoreira2510@users.noreply.github.com>
2023-05-27 15:54:26 -0300

    .

--- HolaMundo/Pais.cpp
+++ HolaMundo/Pais.cpp
@@ -2,35 +2,11 @@
 #include <cstring>
 #include <cstdio>
 #include <iostream>
-#include "funciones.h"
 using namespace std;
 
 void Pais::mostrar(){
     cout << _codigo << " " << _codigo2 << " " << _nombre << " " << _continente << " " << _superficie << " ";
     cout << _poblacion << " " << _independencia << " " << _expectativaDeVida << " " << _capital << endl;
-}
-
-void Pais::cargar(){
-
-    cout << "CODIGO 1: " ;
-    cargarCadena(_codigo, 3);
-    cout << "CODIGO 2: " ;
-    cargarCadena(_codigo2, 2);
-    cout << "NOMBRE: ";
-    cargarCadena(_nombre, 44);
-    cout << "CONTINENTE: " << endl;
-    cargarCadena(_continente, 19);
-    cout << "SUPERFICIE: " << endl;
-    cin >> _superficie;
-    cout << "POBLACION: " << endl;
-    cin >> _poblacion;
-    cout << "INDEPENDENCIA: " << endl;
-    cin >> _independencia;
-    cout << "EXPECTATIVA DE VIDA: " << endl;
-    cin >> _expectativaDeVida ;
-    cout << "CAPITAL: " << endl;
-    cin >> _capital ;
-
 }
 void Pais::setCodigo(const char *codigo){
     strcpy(_codigo, codigo);
--- HolaMundo/Pais.h
+++ HolaMundo/Pais.h
@@ -15,7 +15,6 @@ class Pais{
         int _capital;
     public:
         void mostrar();
-        void cargar();
 
         void setCodigo(const char *);
         void setCodigo2(const char *);
